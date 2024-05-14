6efe665eeeacd4bab29be64f9a8fe4a8c0e8524f
Manuel26190 <manuelfaure26190@gmail.com>
2024-05-14 15:44:02 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
+import { filter } from 'rxjs/operators'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    path: any
+    currentRoute: string = ''
+
+    constructor(private router: Router) {
+        // Router event pour obtenir le chemin actuel
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe((event: any) => {
+                if (event instanceof NavigationEnd) {
+                    this.currentRoute = event.url
+                }
+            })
+    }
 }
